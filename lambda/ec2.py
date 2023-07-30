ba89702d3b76cedb6c795ad452795b423f69663c
Nilay Shrimanwar <shrimanwar92@gmail.com>
2023-07-30 10:07:35 +0530

    Update ec2.py

--- lambda/ec2.py
+++ lambda/ec2.py
@@ -1,49 +1,36 @@
-import string
-import random
 import boto3
 import json
 import botocore
 from typing import TypedDict
+import sys
 
 class InstanceOptions(TypedDict):
     org: str
+    repo: str
     token: str
     ami_id: str
     instance_type: str
     subnet_id: str
     security_group_id: str
     role_name: str
+    label: str
 
 
 class EC2Utils():
-    def __init__(self, credentials) -> None:
-        self.client = boto3.client(
-            'ec2',
-            aws_access_key_id=credentials['AccessKeyId'],
-            aws_secret_access_key=credentials['SecretAccessKey'],
-            aws_session_token=credentials['SessionToken']
-        )
-
-    def generate_label(self):
-        #initializing size of string
-        N = 7
- 
-        # using random.choices()
-        # generating random strings
-        random_string = ''.join(random.choices(string.ascii_lowercase +string.digits, k=N))
-        return str(random_string)
+    def __init__(self) -> None:
+        self.client = boto3.client('ec2')
         
     def start_instance(self, **opts: InstanceOptions):
-        label = self.generate_label()
+        print(f"Runner label: {opts['label']}")
         
         user_data = "\n".join([
             "#!/bin/bash",
-            "mkdir actions-runner && cd actions-runner",
+            "mkdir -p actions-runner && cd actions-runner",
             "case $(uname -m) in aarch64) ARCH='arm64' ;; amd64|x86_64) ARCH='x64' ;; esac && export RUNNER_ARCH=${ARCH}",
-            "curl -O -L https://github.com/actions/runner/releases/download/v2.303.0/actions-runner-linux-${RUNNER_ARCH}-2.303.0.tar.gz",
-            "tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.303.0.tar.gz",
+            "curl -O -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-${RUNNER_ARCH}-2.299.1.tar.gz",
+            "tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.299.1.tar.gz",
             "export RUNNER_ALLOW_RUNASROOT=1",
-            f"./config.sh --url https://github.com/{opts['org']} --token {opts['token']} --labels {label}",
+            f"./config.sh --url https://github.com/{opts['org']}/{opts['repo']} --token {opts['token']} --labels {opts['label']}",
             "./run.sh"
         ])
         
@@ -56,19 +43,30 @@ class EC2Utils():
                 UserData=user_data.encode('ascii'),
                 SubnetId=opts['subnet_id'],
                 SecurityGroupIds=[opts['security_group_id']],
-                IamInstanceProfile={ "Name": opts['role_name'] }    
+                IamInstanceProfile={ "Name": opts['role_name'] },
+                TagSpecifications=[{
+                        'ResourceType': 'instance',
+                        'Tags': [
+                            {'Key': 'AppName', 'Value': 'ec2-self-hosted-runner'},
+                            {'Key': 'Backup', 'Value': 'no'},
+                            {'Key': 'Environment', 'Value': 'poc'},
+                            {'Key': 'Name', 'Value': 'plt-self-hosted-gh-runner-ec2'},
+                            {'Key': 'Product', 'Value': 'plt'},
+                            {'Key': 'GithubRunnerLabel', 'Value': opts['label']},
+                            {'Key': 'DataClassification', 'Value': 'internal'},
+                            {'Key': 'BusinessUnit', 'Value': 'travel.poc'},
+                            {'Key': 'InfraOwner', 'Value': 'manasjit.mohanty@tavisca.com'}
+                        ]
+                    }
+                ]
             )
         except botocore.exceptions.ClientError as error:
             print('AWS EC2 instance starting error');
             print(error)
-            return {
-                "statusCode": 400,
-                "body": str(error)
-            }
+            sys.exit(1)
         else:
             instance_id = response['Instances'][0]['InstanceId']
             print(f"AWS EC2 instance {instance_id} is started")
-            self.add_tag(instance_id, label)
             waiter = self.client.get_waiter('instance_running')
             waiter.wait(InstanceIds=[instance_id])
             print(f"Instance {instance_id} is running")
@@ -77,7 +75,7 @@ class EC2Utils():
                 "statusCode": 200,
                 "body": json.dumps({
                     "InstanceId": instance_id,
-                    "Label": label
+                    "Label": opts['label']
                 })
             }
                 
@@ -86,10 +84,7 @@ class EC2Utils():
             self.client.terminate_instances(InstanceIds=[instance_id])
         except botocore.exceptions.ClientError as error:
             print(error)
-            return {
-                "statusCode": 400,
-                "body": str(error)
-            }
+            sys.exit(1)
         else:
             msg = f"Instance {instance_id} is terminated successfully"
             print(msg)
@@ -97,17 +92,3 @@ class EC2Utils():
                 "statusCode": 200,
                 "body": msg
             }
-        
-    
-    def add_tag(self, instance_id, label):
-        self.client.create_tags(
-            Resources=[
-                instance_id,
-            ],
-            Tags=[
-                {
-                    'Key': 'Github-Runner-Label',
-                    'Value': label
-                },
-            ]
-        )
